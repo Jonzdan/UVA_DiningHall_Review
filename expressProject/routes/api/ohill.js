fc8ad4940deb20f41ed5ede580a12055771d3896
Jonzdan <jonzdan98@gmail.com>
2024-10-18 23:53:02 -0400

    Refactored ohill.js; Pending extrapolation of functions for reusability

--- expressProject/routes/api/ohill.js
+++ expressProject/routes/api/ohill.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const ohillSchema = require('../../models/ohill')
-const axios = require('axios')
-const { csrf } = require('../../auth')
+const express = require('express');
+const router = express.Router();
+const ohillSchema = require('../../models/ohill');
+const axios = require('axios');
+const { csrf } = require('../../auth');
 
-const ohillstations = { //tentative -- seems to change
+const ohillStations = { //tentative -- seems to change
     "22869": "Copper Hood",
     "22868": "The Iron Skillet", //Was also Umami
     "22867": "The Iron Skillet", //was Umami/Stir-Fry
@@ -18,187 +18,283 @@ const ohillstations = { //tentative -- seems to change
     "22872": "Savory Stack",
     "39894": "Hummus Vegetable Wrap",
     "22876": "Umami"
-}
+};
 
 //22876 is for breakfast UMAMI....
 //22868/22867 change for lunch/dinner (only ironskillet for breakrfast, then it is both)
 
 router.get('/', async(req, res) => {
     try {
-        let date = getCurDateAsString()
-        let time = getOhillTimeFrame(new Date().getDay(), getCurHour())
-        const data = await ohillSchema.find({activeDate: {$in : [date]}, 'item.timeFrame': time}, {_id: 0, "item.itemReview.reviews": 0, "item.itemReview.starsLength": 0, activeDate: 0,}).sort({
+        let curDate = getCurDateAsString();
+        let ohillTimeFrame = getOhillTimeFrame(new Date().getDay(), getCurHour());
+        let data = await ohillSchema.find(
+            {
+                activeDate: {
+                    $in : [curDate],
+                },
+                'item.timeFrame': ohillTimeFrame,
+            },
+            {
+                _id:                           0,
+                "item.itemReview.reviews":     0,
+                "item.itemReview.starsLength": 0,
+                activeDate:                    0,
+            }
+        ).sort({
             "item.itemReview.starsLength": -1,
-        })
-        if (data && Object.keys(data).length === 0) {
-            await getData()
-            const data1 = await ohillSchema.find({activeDate: {$in : [date]}, 'item.timeFrame': time}, {_id: 0, "item.itemReview.reviews": 0, "item.itemReview.starsLength": 0, activeDate: 0,}).sort({
-            "item.itemReview.starsLength": -1,
-        })
-            res.json(data1)
+        });
+        if (!data) {
+            res.status(500).end();
+            return;
         }
-        else {
-            res.json(data)
+        if (Object.keys(data).length === 0) {
+            await getData();
+            data = await ohillSchema.find(
+                {
+                    activeDate: {
+                        $in : [curDate],
+                    },
+                    'item.timeFrame': ohillTimeFrame,
+                },
+                {
+                    _id:                           0,
+                    "item.itemReview.reviews":     0,
+                    "item.itemReview.starsLength": 0,
+                    activeDate:                    0,
+                }
+            ).sort({
+                "item.itemReview.starsLength": -1,
+            });
         }
-
+        res.json(data);
     }
     catch (err) {
-        res.status(500).json({msg: err.msg})
+        res.status(500).json({
+            msg: err.msg
+        });
     }
 })
 
-router.post('/', csrf ,async (req, res) => {
-    const dataObj = req.body
-    try { //add validation later
-        let date = getCurDateAsString(); let time = getOhillTimeFrame(new Date().getDay(), getCurHour())
-        if (dataObj["Content"].length < 50 || dataObj["Content"] === undefined || dataObj["APP-STARS"] <= 0 || dataObj["APP-STARS"] > 5) { res.status(400).json({msg: "unknown parameters"}); return }
-        const temp = await ohillSchema.findOneAndUpdate({stationName: dataObj.stationName, activeDate: date, "item.timeFrame": {$in: [time]}, "item.itemName": dataObj.itemName, "item.itemDesc": dataObj.itemDesc}, {$push: {"item.itemReview.stars": dataObj['APP-STARS'], "item.itemReview.reviews":dataObj['Content']}, $inc: {"item.itemReview.starsLength": 1}}, {returnOriginal: false})
-        //console.log("TEMP: \n", temp)
-        res.json("Updated")
+router.post('/', csrf, async (req, res) => {
+    const dataObj = req.body;
+    try {
+        const date = getCurDateAsString();
+        const time = getOhillTimeFrame(new Date().getDay(), getCurHour());
+        if (dataObj["Content"].length < 50 || dataObj["Content"] === undefined || dataObj["APP-STARS"] <= 0 || dataObj["APP-STARS"] > 5) {
+            res.status(400).json({
+                msg: "unknown parameters"
+            });
+            return;
+        }
+        const result = await ohillSchema.findOneAndUpdate(
+            {
+                stationName:     dataObj.stationName,
+                activeDate:      date,
+                "item.timeFrame": {
+                    $in: [time]
+                },
+                "item.itemName": dataObj.itemName,
+                "item.itemDesc": dataObj.itemDesc,
+            },
+            {
+                $push: {
+                    "item.itemReview.stars":   dataObj['APP-STARS'],
+                    "item.itemReview.reviews": dataObj['Content']
+                },
+                $inc: {
+                    "item.itemReview.starsLength": 1
+                }
+            },
+            {
+                returnOriginal: false
+            }
+        );
+        res.json("Updated");  // Will not touch for simplicity purposes
     }
     catch (err) {
-        console.log(err)
-        res.status(503).json({msg: err.msg})
+        console.log(err);
+        res.status(500).json({
+            msg: err.msg
+        });
     }
 })
 
-
-function removeSpecialChar(s) {
-    if (!(s)) {
-        return
+/** 
+ * Replaces unicode \\u0026 and \\u0027 with & and ' respectively for web content.
+*/
+function removeSpecialChar(string) {
+    if (!string) {
+        return string;
     }
-    if (s.indexOf("\\u0026") != -1) {
-        let first = s.indexOf("\\u0026")
-        let end = first + 7
-        s = s.slice(0, first) + '& ' + s.slice(end)
-    }
-    if (s.indexOf("\\u0027") != -1) {
-        let first = s.indexOf("\\u0027")
-        let end = first + 7
-        s = s.slice(0, first) + "'" + s.slice(end)
-    }
-
-    return s
+    return string.replace(/\\u0026/g, '&').replace(/\\u0027/g, "'");
 }
 
 function getCurDateAsString() {
-    const a = new Date()
-    let month = (a.getMonth() + 1).toString()
-    if (month.length === 1) {
-        month = '0' + month
-    }
-    let day = a.getDate().toString()
-    if (day.length === 1) {
-        day = '0' + day
-    }
-    let year = a.getFullYear().toString()
-    let val = year+month+day
-    return val
+    return new Date().toISOString().slice(0, 10).replace(/-/g, '');
 }
 
 function getCurHour() {
-    const a = new Date().toString()
-    let start = a.indexOf(":")-2
-    let se = start + 2
-    let ss = se + 1
-    let sse = ss + 2
-    return a.slice(start, se) + a.slice(ss, sse)
+    let hour = new Date().getHours().toString();
+    if (hour.length === 1) {
+        hour = '0' + hour;
+    }
+    return hour;
 }
 
 function getOhillTimeFrame(date, time) {
-    if (date == 0 || date == 6) { //sat or sun
-        if (time >= 1100 && time < 1400) {
-            return 'Brunch (8am-2:15pm)'
+    if (date === 0 || date === 6) {
+        if (time >= 800 && time < 1415) {
+            return 'Brunch (8am-2:15pm)';
         }
-        else if (time >= 1700 && time < 2000 ) {
-            return 'Dinner (5pm-8pm)'
-        }
-        else if (time >= 1415 && time < 1700) {
-            return 'Afternoon Snack (2:15pm-5pm)'
-        }
-        else {
-            return 'Unavailable'
+        if (time < 800 || time >= 2000) {
+            return 'Unavailable'; 
         }
     }
-    else {
-        if (time >= 0700 && time < 1100) {
-            return 'Breakfast (7am-11am)'
-        }
-        else if (time >= 1100 && time < 1415) {
-            return 'Lunch (11am-2:15pm)'
-        }
-        else if (time >= 1415 && time < 1700) {
-            return 'Afternoon Snack (2:15pm-5pm)'
-        }
-        else if (time >= 1700 && time < 2000) {
-            return 'Dinner (5pm-8pm)'
-        }
-        else {
-            return 'Unavailable'
-        }
+    if (time >= 700 && time < 1100) {
+        return 'Breakfast (7am-11am)';
+    } else if (time >= 1100 && time < 1415) {
+        return 'Lunch (11am-2:15pm)';
+    } else if (time >= 1415 && time < 1700) {
+        return 'Afternoon Snack (2:15pm-5pm)';
+    } else if (time >= 1700 && time < 2000) {
+        return 'Dinner (5pm-8pm)';
+    } else {
+        return 'Unavailable';
     }
 }
 
-async function getData() { //figure out a faster way --> perhaps load only 3, then lazy load the rest?
-    const res = await axios.get('https://virginia.campusdish.com/LocationsAndMenus/ObservatoryHillDiningRoom')
+function getFoodProducts(data) {
+    let iterator = 0;
+    const foodProducts = [];
+    while (iterator < data.length) {
+        const newProductIndex = data.indexOf('"Product":', iterator);
 
-    //console.log(res.data.slice(80000,200000))
-    //const ind = res.data.indexOf("model")
-    const endInd = res.data.indexOf("/Cart") + 8 //looks like MarketingName, and ShortDescription, DisplayName (Entrees...), isActive... , Product":{ and more like Contains Fish or whatnot
-    //let data = res.data.slice(ind, endInd)
-    let data = res.data
-    let iterator = 0 //or ind
-    let curDate = getCurDateAsString().trim()
-    let timeFrame = getOhillTimeFrame(new Date().getDay(), getCurHour())
-    const size = data.length;
-    while (data.indexOf('"Product":', iterator) !== -1 && iterator < size) {
-        let shopId = data.indexOf('"StationId":', iterator) + 13
-        let end = data.indexOf('"', shopId)
-        let stationId = data.slice(shopId, end)
-        let start = data.indexOf('"Product":{', end)
-        let mn = data.indexOf("MarketingName", start) + 16
-        let mne = data.indexOf('"', mn)
-        let resultString = data.slice(mn, mne)
-        let begin = data.indexOf("ShortDescription", mne+1)
-        begin = data.indexOf('"', begin + 18) + 1
-        let sde = data.indexOf('"', begin+3)
-        let descriptionString = data.slice(begin, sde)
-        descriptionString = removeSpecialChar(descriptionString)
-        resultString = removeSpecialChar(resultString)
-        //* There potentially could be an error in adding to existing ones* fixed** 
-        let existing = await ohillSchema.find({ "item.itemName" : resultString, "item.timeFrame": timeFrame, "stationName" : ohillstations[stationId]})
-            //console.log(resultString, stationId, ohillstations[stationId])
-        const objLength = Object.keys(existing).length
-        if (existing != undefined && existing != null && objLength !== 0 && existing[objLength-1].activeDate[existing[objLength-1].activeDate.length-1]) {
-            await ohillSchema.updateOne({_id: existing[objLength-1]._id, "item.timeFrame" : timeFrame}, {$push: {activeDate: curDate}})
+        if (newProductIndex === -1) {
+            break;
         }
-        else {
-            if (!(stationId in ohillstations)) { /* We should audit this, and then stop the process */ return }
-            const obj = {
-                stationName: ohillstations[stationId],
+
+        const newStationIdIndex = data.indexOf('"StationId":', iterator) + 13;
+        const endOfNewStationIdIndex = data.indexOf('"', newStationIdIndex);
+        const newStationId = data.slice(newStationIdIndex, endOfNewStationIdIndex);
+
+        const startOfNewProductIndex = data.indexOf('"Product":{', endOfNewStationIdIndex);
+        const startOfMarketingNameIndex = data.indexOf("MarketingName", startOfNewProductIndex) + 16;
+        const endOfMarketingNameIndex = data.indexOf('"', startOfMarketingNameIndex);
+        const newMarketingName = removeSpecialChar(data.slice(startOfMarketingNameIndex, endOfMarketingNameIndex));
+
+        const startOfShortDescriptionObjectIndex = data.indexOf("ShortDescription", endOfMarketingNameIndex + 1);
+        const startOfShortDescriptionIndex = data.indexOf('"', startOfShortDescriptionObjectIndex + 18) + 1;
+        const endOfShortDescriptionIndex = data.indexOf('"', startOfShortDescriptionIndex + 3);
+        const newShortDescription = removeSpecialChar(data.slice(startOfShortDescriptionIndex, endOfShortDescriptionIndex));
+
+        foodProducts.push({
+            stationId:        newStationId,
+            marketingName:    newMarketingName,
+            shortDescription: newShortDescription,
+        });
+
+        itemNames.add(newMarketingName);
+
+        iterator = endOfShortDescriptionIndex + 1;
+    }
+    return foodProducts;
+}
+
+/**
+ * 
+ * @param {*} data
+ * @param {*} existingFoodProductsMapping
+ * A Map Object of Key: `${item.itemName}-${stationName}`, Value: { MongoDB Document Item }
+ * @param {*} curDate 
+ * @param {*} timeFrame 
+ * @param {*} stationMapping 
+ * @returns An array of MongoDB write operations
+ */
+function bulkProcessFoodProducts(data, existingFoodProductsMapping, curDate, timeFrame, stationMapping) {
+    const foodProducts = getFoodProducts(data);
+    const bulkOperations = [];
+    for (const products of foodProducts) {
+        const { stationId, marketingName, shortDescription } = products;
+        const existingProduct = existingFoodProductsMapping.get(`${products.item.itemName}-${products.stationName}`)
+
+        if (existingProduct) {
+            bulkOperations.push({
+                updateOne: {
+                    filter: {
+                        _id: existingProduct._id,
+                    },
+                    update: {
+                        $push: {
+                            activeDate: curDate,
+                        }
+                    },
+                }
+            });
+        } else {
+            if (!(stationId in stationMapping)) {
+                console.log(`${stationId} is unknown`);
+                return;
+            }
+
+            const stationProduct = {
+                stationName: stationMapping[stationId],
                 item: {
-                    itemName: resultString,
-                    itemDesc: descriptionString,
+                    itemName:  marketingName,
+                    itemDesc:  shortDescription,
                     timeFrame: timeFrame,
                     itemReview: {
-                        stars: [],
+                        stars:       [],
                         starsLength: 0
                     }
                 },
                 activeDate: [curDate]
+            };
+
+            if (stationProduct && stationProduct.item.timeFrame !== 'Unavailable') {
+                bulkOperations.push({
+                    insertOne: {
+                        document: stationProduct,
+                    }
+                });
             }
-            if (obj && obj.item.timeFrame !== 'Unavailable') {
-                let ohill1 = new ohillSchema(obj)
-                await ohill1.save()
-            }
-            
         }
-        iterator = sde + 1
     }
-    //console.log(currentOhillDiningOptions
-
-
-
+    return bulkOperations;
 }
 
-module.exports = router
+async function getData() { //figure out a faster way --> perhaps load only 3, then lazy load the rest?
+    const res = await axios.get('https://virginia.campusdish.com/LocationsAndMenus/ObservatoryHillDiningRoom');
+    const curDate = getCurDateAsString().trim();
+    const timeFrame = getOhillTimeFrame(new Date().getDay(), getCurHour());
+    const itemNames = new Set();
+    const existingFoodProductsMapping = new Map();
+
+    const existingFoodProducts = await ohillSchema.find({
+        "item.timeFrame": timeFrame,
+        stationName: {
+            $in: Object.values(ohillStations),
+        },
+        "item.itemName": {
+            $in: [...itemNames],
+        }
+    });
+
+    for (const product of existingFoodProducts) {
+        existingFoodProductsMapping.set(
+            `${product.item.itemName}-${product.stationName}`,
+            product,
+        );
+    }
+
+    bulkOperations = bulkProcessFoodProducts(res.data, existingFoodProductsMapping, curDate, timeFrame, ohillStations);
+
+    if (bulkOperations.length > 0) {
+        try {
+            ohillSchema.bulkWrite(bulkOperations);
+        } catch (error) {
+            console.error("Bulk write failed:", error)
+        }
+    }
+}
+
+module.exports = router;
