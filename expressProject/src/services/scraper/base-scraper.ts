d910865501585ed2ed4b50a6b5da51eda5996b98
Jonzdan <jonzdan98@gmail.com>
2025-09-05 12:46:32 -0400

    removed dev settings

--- expressProject/src/services/scraper/base-scraper.ts
+++ expressProject/src/services/scraper/base-scraper.ts
@@ -116,7 +116,6 @@ export class DiningHallDataParser {
                 throw new DiningHallDataParserError("Bulk write failed");
             }
         }
-        return this.mapStationIdToFoodSchema(foodProducts, stationIdsToNameMap);
         return undefined;
     }
 
